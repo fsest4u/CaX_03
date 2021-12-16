ad335d039e777a73d673582c18f9274254eb2042
fsest4u <fsest4u@gmail.com>
2021-12-16 12:37:16 +0900

    [install] 설치 과정 및 위치 수정

--- installer/build.bat
+++
@@ -1,9 +0,0 @@
-
-
-rmdir /s .\src
-
-mkdir .\src
-
-copy ..\build-CaX-Desktop_Qt_5_12_10_MSVC2017_32bit-Release\release\CaX.exe .\src
-
-c:\apps\Qt5.12.10\5.12.10\msvc2017\bin\windeployqt.exe .\src\CaX.exe
