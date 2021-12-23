fdb400e1f4d8fbba69620dfed8f492bb85c62920
fsest4u <fsest4u@gmail.com>
2021-12-23 19:28:52 +0900

    [log] 로그를 파일로 저장

--- CaX/util/log.cpp
+++ CaX/util/log.cpp
@@ -1,20 +1,12 @@
 #include <QFile>
 #include <QCoreApplication>
+#include <QDateTime>
 
 #include "log.h"
 
 void LogCax(int nLevel, const char *filename, const char *funcname, const int linenum, const char *fmt, ...)
 {
 	Q_UNUSED(filename)
-
-	QString appPath = QCoreApplication::applicationDirPath();
-	QFile file(appPath + "/log.txt");
-	if(!file.open(QIODevice::Append | QIODevice::Text))
-	{
-		return;
-	}
-
-	QTextStream out(&file);
 
 	va_list args;
 	va_start(args, fmt);
@@ -26,27 +18,55 @@ void LogCax(int nLevel, const char *filename, const char *funcname, const int li
 	{
 	case DEBUG:
 		qDebug()	<< "DEB [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
-		out			<< "DEB [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
 		break;
 	case INFO:
 		qInfo()		<< "INF [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
-		out			<< "INF [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
 		break;
 	case WARNING:
 		qWarning()	<< "WAR [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
-		out			<< "WAR [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
 		break;
 	case CRITICAL:
 		qCritical()	<< "CRI [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
-		out			<< "CRI [" << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
 		break;
 	default:
 		qDebug()	<< strMsg << "\n";
+		break;
+	}
+
+#if 1
+	QString appPath = QCoreApplication::applicationDirPath();
+	QFile file(appPath + "/log.txt");
+	if(!file.open(QIODevice::Append | QIODevice::Text))
+	{
+		return;
+	}
+
+	QString curtime = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
+
+	QTextStream out(&file);
+
+	switch (nLevel)
+	{
+	case DEBUG:
+		out			<< "DEB [" << curtime << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
+		break;
+	case INFO:
+		out			<< "INF [" << curtime << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
+		break;
+	case WARNING:
+		out			<< "WAR [" << curtime << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
+		break;
+	case CRITICAL:
+		out			<< "CRI [" << curtime << funcname << "]\t[" <<  linenum << "]\t" << strMsg << "\n";
+		break;
+	default:
 		out			<< strMsg << "\n";
 		break;
 	}
 
+	file.close();
+#endif
+
 	va_end(args);
 
-	file.close();
 }
