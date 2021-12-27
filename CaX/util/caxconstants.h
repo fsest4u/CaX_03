23f02063bd0aaf9920b7841ce3678b281c04a527
fsest4u <fsest4u@gmail.com>
2021-12-27 13:45:25 +0900

    version 1.1

--- CaX/util/caxconstants.h
+++ CaX/util/caxconstants.h
@@ -3,7 +3,7 @@
 
 #include <QCoreApplication>
 
-#define	APP_VERSION					"0.11"
+#define	APP_VERSION					"1.1"
 #define FIRMWARE_MIN_VERSION		1720
 //////////////////////////////////////////////////
 // ssdp
